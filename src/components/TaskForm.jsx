e6a6d77ff36aed06eef5e35dcc650ddcdceaf5e2
stevekahanya <wakahusteve@gmail.com>
2026-02-23 04:26:59 +0300

    update task

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,17 +3,21 @@ import { TaskContext } from "../context/TaskContext";
 
 function TaskForm() {
   const [taskName, setTaskName] = useState("");
+  const inputId = useId(); // Generate unique ID
+  const { addTask } = useContext(TaskContext); // Consume Context
 
   function handleSubmit(e) {
     e.preventDefault();
     if (taskName.trim() === "") return;
+    addTask(taskName); // Trigger context function
     setTaskName("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>New Task:</label>
+      <label htmlFor={inputId}>New Task:</label>
       <input
+        id={inputId}
         type="text"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
