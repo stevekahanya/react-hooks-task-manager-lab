7d89a9f7cd4f17df395107ca1aa9ef4eea8f6163
stevekahanya <wakahusteve@gmail.com>
2026-02-24 10:22:10 +0300

    fix import path for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 import { TaskProvider } from './context/TaskContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
